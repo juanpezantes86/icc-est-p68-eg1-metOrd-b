081dc716848b7c1d93f952b89c70c6943c182879
Sebastian Arenillas <sebasarenillas10@gmail.com>
2026-05-18 17:24:33 -0500

    Parc4

--- src/controllers/MovieController.java
+++ src/controllers/MovieController.java
@@ -13,20 +13,18 @@ public class MovieController {
      * @param movies Arreglo de películas a ordenar
      */
     public void sortByTitle(Movie[] movies) {
-        int tam = movies.length;
+    int tam = movies.length;
+
         
-        for(int i=0; i<tam - 1; i++) {
-            int indiceMenor = i;
-            for(int j=i+1; j<tam; j++) {
-                if(movies[j].getTitle().compareTo(movies[indiceMenor].getTitle())<0) {
-                    indiceMenor = j;
-                }
+        for (int i = 1; i < tam; i++) {
+            Movie actual = movies[i]; 
+            int j = i - 1;
+
+            while (j >= 0 && movies[j].getTitle().compareTo(actual.getTitle()) > 0) {
+                movies[j + 1] = movies[j];
+                j--;
             }
-            if(indiceMenor != i) {
-                Movie temp = movies[i];
-                movies[i] = movies[indiceMenor];
-                movies[indiceMenor] = temp;
-            }
+            movies[j + 1] = actual;
         }
 
     }
